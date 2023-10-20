9106f109ded17cfd157c9568ed032d502d159601
trillcy <trillcy.ltd@gmail.com>
2023-10-20 13:02:51 +0300

    fix v6

--- src/repositories/users-db-repository.ts
+++ src/repositories/users-db-repository.ts
@@ -248,14 +248,14 @@ export const usersRepository = {
     return result
   },
   async delete(id: string): Promise<boolean | null> {
-    // try {
-    const result = await UserModel.deleteOne({ _id: new ObjectId(id) })
-    console.log('258++users.repo--результат удвления юзера', result)
+    try {
+      const result = await UserModel.deleteOne({ _id: new ObjectId(id) })
+      console.log('258++users.repo--результат удвления юзера', result)
 
-    return true //result.deletedCount === 1
-    // } catch (e) {
-    //   return null
-    // }
+      return result.deletedCount === 1
+    } catch (e) {
+      return true
+    }
   },
 
   async create(newElement: UserDBType): Promise<string> {
